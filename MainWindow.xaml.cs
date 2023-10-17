73c3086d04f1c724cb7be534628809bb5c8543ac
Olga Matveeva <matveevaolga2882@gmail.com>
2023-10-17 11:44:51 +0300

    created main window

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -10,7 +10,6 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace FormProject
@@ -23,31 +22,6 @@ namespace FormProject
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private bool IsFormValid(string userLogin, string userPassword)
-        {
-            DBFunctions dBFunctions = new DBFunctions();
-            bool isRegistered = dBFunctions.IsRegistered(userLogin);
-            string loginmessage = "Введите логин";
-            string passwordmessage = "Введите пароль";
-            if (isRegistered) { loginmessage = "Пользователь с таким логином уже зарегестрирован"; }
-            if (userPassword.Length < 5) { passwordmessage = "Введенный пароль слишком ненадежен"; }
-            loginError.Text = loginmessage, passwordError.Text = passwordmessage;
-            if (isRegistered | userPassword.Length < 5) { return false; }
-            if (dBFunctions.Register(userLogin, userPassword)) { return true; }
-            else { return false; }
-        }
-
-        private void SendForm(object sender, EventArgs e)
-        {
-            message.Text = "Заполните форму для регистрации";
-            string userLogin = login.Text.Trim();
-            string userPassword = password.Password.Trim();
-            if (IsFormValid(userLogin, userPassword)) { message.Text = "Вы успешно зарегестрировались"; }
-            else { message.Text = "Произошла ошибка, повторите регистрацию"; }
-            login.Text = "";
-            password.Password = "";
         }
     }
 }
