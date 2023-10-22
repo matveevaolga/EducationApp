d4d7810e78c313b7af91e6808d2ce666b3f8a0d9
Olga Matveeva <matveevaolga2882@gmail.com>
2023-10-22 03:29:01 +0300

    created a profile layout

---
+++ UserProfile.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FormProject
+{
+    /// <summary>
+    /// Логика взаимодействия для UserProfile.xaml
+    /// </summary>
+    public partial class UserProfile : Window
+    {
+        public UserProfile()
+        {
+            InitializeComponent();
+            statsBorder.Visibility = Visibility.Visible;
+            editBorder.Visibility = Visibility.Collapsed;
+            settingsBorder.Visibility = Visibility.Collapsed;
+        }
+
+        private void ShowStats(object sender, EventArgs e)
+        {
+            statsBorder.Visibility = Visibility.Visible;
+            editBorder.Visibility = Visibility.Collapsed;
+            settingsBorder.Visibility = Visibility.Collapsed;
+        }
+
+        private void ShowEdit(object sender, EventArgs e)
+        {
+            statsBorder.Visibility = Visibility.Collapsed;
+            editBorder.Visibility = Visibility.Visible;
+            settingsBorder.Visibility = Visibility.Collapsed;
+        }
+
+        private void ShowSettings(object sender, EventArgs e)
+        {
+            statsBorder.Visibility = Visibility.Collapsed;
+            editBorder.Visibility = Visibility.Collapsed;
+            settingsBorder.Visibility = Visibility.Visible;
+        }
+    }
+}
