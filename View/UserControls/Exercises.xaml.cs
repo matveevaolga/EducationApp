7d98bc33d4fee6339247f2b752f2f5a42b695f47
Olga Matveeva <matveevaolga2882@gmail.com>
2023-11-10 22:23:53 +0300

    added the ability to select the type of exercise using combobox

--- View/UserControls/Exercises.xaml.cs
+++ View/UserControls/Exercises.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using FormProject.View.UserControls.ExercisesUCs;
 
 namespace FormProject.View.UserControls
 {
@@ -23,6 +24,26 @@ namespace FormProject.View.UserControls
         public Exercises(string login)
         {
             InitializeComponent();
+        }
+
+        private void WriteCodeChosen(object sender, RoutedEventArgs e)
+        {
+            switchExerciseUC.Content = new WriteCode();
+        }
+
+        private void YourAnswerChosen(object sender, RoutedEventArgs e)
+        {
+            switchExerciseUC.Content = new YourAnswer();
+        }
+
+        private void ChooseCorrectChosen(object sender, RoutedEventArgs e)
+        {
+            switchExerciseUC.Content = new ChooseCorrect();
+        }
+
+        private void InsertTheMissingChosen(object sender, RoutedEventArgs e)
+        {
+            switchExerciseUC.Content = new InsertTheMissing();
         }
     }
 }
