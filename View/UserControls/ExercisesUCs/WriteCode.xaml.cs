971f4909723aae698aeb20a3ad2bd6a316a15cdf
Olga Matveeva <matveevaolga2882@gmail.com>
2023-12-09 18:15:48 +0300

    A form in which the user can write code - WriteCode content control

--- View/UserControls/ExercisesUCs/WriteCode.xaml.cs
+++ View/UserControls/ExercisesUCs/WriteCode.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Scripting.Hosting;
+using IronPython.Hosting;
 
 namespace FormProject.View.UserControls.ExercisesUCs
 {
@@ -41,7 +43,33 @@ namespace FormProject.View.UserControls.ExercisesUCs
             ShowExerciseDesc();
         }
 
-        public void ShowExerciseDesc() { }
+        public void ShowExerciseDesc()
+        {
+            DockPanel exercisePanel = new DockPanel();
+            exercisePanel.LastChildFill = true;
+
+            TextBlock description = new TextBlock();
+            description.Text = exerciseData["Описание"];
+            description.Style = Application.Current.FindResource("TextBlockStyle") as Style;
+            description.TextWrapping = TextWrapping.Wrap;
+            DockPanel.SetDock(description, Dock.Top);
+            description.HorizontalAlignment = HorizontalAlignment.Left;
+            exercisePanel.Children.Add(description);
+
+            TextBox scriptForm = new TextBox();
+            scriptForm.TextWrapping = TextWrapping.Wrap;
+            DockPanel.SetDock(description, Dock.Top);
+            scriptForm.Style = Application.Current.FindResource("TextBoxStyle") as Style;
+            scriptForm.AcceptsReturn = true;
+            scriptForm.AcceptsTab = true;
+            exercisePanel.Children.Add(scriptForm);
+
+            ScrollViewer scrollViewer = new ScrollViewer();
+            scrollViewer.VerticalScrollBarVisibility = ScrollBarVisibility.Auto;
+            scrollViewer.Content = exercisePanel;
+            scrollViewer.HorizontalContentAlignment = HorizontalAlignment.Stretch;
+            exerciseDesc.Content = scrollViewer;
+        }
 
         private void CheckIfCorrect(object sender, EventArgs e) { }
     }
