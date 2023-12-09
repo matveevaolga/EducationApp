7d0ffdd6362a5a18407b7a2fea241435efef6c60
Olga Matveeva <matveevaolga2882@gmail.com>
2023-12-09 21:18:05 +0300

    Checking if the output of the user's program is correct - funcion CheckIfCorrect created - function ProcessInputScripts created

--- View/UserControls/ExercisesUCs/WriteCode.xaml.cs
+++ View/UserControls/ExercisesUCs/WriteCode.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Microsoft.Scripting.Hosting;
 using IronPython.Hosting;
+using System.Reflection.Emit;
 
 namespace FormProject.View.UserControls.ExercisesUCs
 {
@@ -71,6 +72,32 @@ namespace FormProject.View.UserControls.ExercisesUCs
             exerciseDesc.Content = scrollViewer;
         }
 
-        private void CheckIfCorrect(object sender, EventArgs e) { }
+        private void CheckIfCorrect(object sender, EventArgs e)
+        {
+            ScrollViewer scrollViewer = exerciseDesc.Content as ScrollViewer;
+            DockPanel exercisePanel = scrollViewer.Content as DockPanel;
+            string input = "";
+            foreach (var element in exercisePanel.Children)
+            {
+                if (element is TextBox textBox) input += textBox.Text;
+            }
+            bool result = ProcessInputScript(input);
+        }
+
+        private bool ProcessInputScript(string input)
+        {
+            string[] answers = exerciseData["Ответ"].Split(new string[] {"#\n"}, StringSplitOptions.None);
+            string[] tests = exerciseData["Дополнительный контент"].Split();
+            ScriptEngine engine = Python.CreateEngine();
+            ScriptScope scope = engine.CreateScope();
+            engine.Execute(input, scope);
+            dynamic fib = scope.GetVariable("fib");
+            for (int i = 0; i < answers.GetLength(0); i++)
+            {
+                dynamic output = fib(int.Parse(tests[i]));
+                if (output != answers[i]) { Console.WriteLine(tests[i]); return false; }
+            }
+            return true;
+        }
     }
 }
