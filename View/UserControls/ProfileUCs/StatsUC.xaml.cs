6adaaab25fbfb5c546bab26d570a59d25b90620e
matveevaolga <matveevaolga2882@gmail.com>
2024-02-10 01:35:50 +0300

    Modifying userProfile markup
    - (in StatsUC) creating tables with exercises solved and created by user in markup and connecting their content with ExerciseDescription objects
    - moving user controls connected to user profile to ProfileUCs folder

---
+++ View/UserControls/ProfileUCs/StatsUC.xaml.cs
@@ -0,0 +1,56 @@
+﻿using FormProject.Controller;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Controls;
+
+namespace FormProject.View.UserControls
+{
+    /// <summary>
+    /// Логика взаимодействия для StatsUC.xaml
+    /// </summary>
+    public partial class StatsUC : UserControl
+    {
+        public class ExerciseDescription
+        {
+            public int Id { get; set; }
+            public string Type { get; set; }
+            public int Exp { get; set; }
+            public string Date { get; set; }
+            public ExerciseDescription(int id, string type, int exp, string date)
+            {
+                Id = id;
+                Type = type;
+                Exp = exp;
+                Date = date;
+            }
+        }
+        public string uActive { get; set; }
+        public string uMaxSession { get; set; }
+        public string uSolvedAmount { get; set; }
+        public string uTopicsAmount { get; set; }
+        List<ExerciseDescription> solvedExercises;
+        public List<ExerciseDescription> SolvedExercises
+        {
+            get { return solvedExercises; }
+            set { solvedExercises = value; }
+        }
+        List<ExerciseDescription> createdExercises;
+        public List<ExerciseDescription> CreatedExercises
+        {
+            get { return createdExercises; }
+            set { createdExercises = value; }
+        }
+        public StatsUC(string login)
+        {
+            InitializeComponent();
+            uActive = DBHelpFunctional.HelpGetStatsField(login, "active");
+            uMaxSession = DBHelpFunctional.HelpGetStatsField(login, "maxSession");
+            uSolvedAmount = DBHelpFunctional.HelpGetStatsField(login, "solvedAmount");
+            uTopicsAmount = DBHelpFunctional.HelpGetStatsField(login, "coveredTopicsAmount");
+            //FillSolved();
+            //FillCreated();
+            DataContext = this;
+        }
+    }
+}
